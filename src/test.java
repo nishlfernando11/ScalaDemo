3a9b0d9a7668843f8fbf50bd1d3ce375ebb63568
nishlfernando11 <nlfernando11@gmail.com>
2016-08-28 19:26:18 +0530

    test

---
+++ src/test.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nish
+ * Date: 8/28/16
+ * Time: 7:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+    public static void main(){
+        System.out.println("test");
+    }
+}
